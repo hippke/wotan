fb24da07feaab32a5de33789984f967a93fcc386
Michael Hippke <hippke@users.noreply.github.com>
2019-03-05 16:45:01 +0100

    Update biweight.py

--- biweight.py
+++ biweight.py
@@ -1,6 +1,6 @@
 import numpy
 import numba
-
+from numpy import pi, sin, exp
 
 @numba.jit(fastmath=True, parallel=False, nopython=True)
 def roll(a, shift):
@@ -17,7 +17,7 @@ def roll(a, shift):
 
 
 @numba.jit(fastmath=True, parallel=False, nopython=True)
-def biweight_location(data, c=6.0):
+def biweight_location(data, c=4.685):  # 6 default; 4.685 for 95% efficiency
     M = numpy.median(data)
     d = data - M
     mad = numpy.median(numpy.abs(data - numpy.median(data)))
@@ -31,6 +31,43 @@ def biweight_location(data, c=6.0):
 
 
 @numba.jit(fastmath=True, parallel=False, nopython=True)
+#https://cran.r-project.org/web/packages/robustbase/vignettes/psi_functions.pdf
+def welsh_location(data, c=2.11):
+    M = numpy.median(data)
+    d = data - M
+    mad = numpy.median(numpy.abs(data - numpy.median(data)))
+    if mad == 0:
+        return M
+    u = d / (c * mad)
+    mask = (numpy.abs(u) >= 1)
+    u = exp(-u**2 / 2)
+    u[mask] = 0
+    return M + (d * u).sum() / u.sum()
+
+
+@numba.jit(fastmath=True, parallel=False, nopython=True)
+def andrewsinewave_location(data, c=1.339):
+    M = numpy.median(data)
+    d = data - M
+    mad = numpy.median(numpy.abs(data - numpy.median(data)))
+    if mad == 0:
+        return M
+    u = d / (c * mad)
+    u[u==0] = 1e-100  # avoid division by zero
+    mask = (numpy.abs(u) >= pi)
+    u = sin(u) / u
+    u[mask] = 0
+    return M + (d).sum() / u.sum()
+
+"""
+data = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20])
+print(biweight_location(data, c=4.685))
+#print(biweight_location(data, c=4.685))
+print(andrewsinewave_location(data, c=4.685))
+print(welsh_location(data))
+"""
+
+@numba.jit(fastmath=True, parallel=False, nopython=True)
 def segment(t, y, window):
     mean = numpy.full(len(t), numpy.nan)
     for i in range(len(t)-1):
@@ -38,7 +75,9 @@ def segment(t, y, window):
             mean_segment = numpy.nan
             l = numpy.argmax(t > t[i] - window/2)
             h = numpy.argmax(t > t[i] + window/2)
-            mean_segment = biweight_location(y[l:h])
+            #mean_segment = biweight_location(y[l:h])
+            #mean_segment = andrewsinewave_location(y[l:h])
+            mean_segment = welsh_location(y[l:h])
             mean[i+int(window/2)] = mean_segment
     return roll(mean, int(window/2))
 
