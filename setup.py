2deb17adaecede88cd0f2f4dc96a026e5fca2d97
Michael Hippke <hippke@users.noreply.github.com>
2019-04-09 22:11:23 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from os import path
+
+# If Python3: Add "README.md" to setup. 
+# Useful for PyPI (pip install wotan). Irrelevant for users using Python2
+try:
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ' '
+
+    
+setup(name='wotan',
+    version=1,
+    description='Wōtan is a free and open source algorithm to automagically remove stellar trends from light curves for exoplanet transit detection',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hippke/wotan',
+    author='Michael Hippke',
+    author_email='michael@hippke.org',
+    license='MIT',
+    packages=['wotan'],
+    install_requires=[
+        'numpy',
+        'numba'
+        ]
+)
