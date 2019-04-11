1cd6324eb57ee75f7e9aad4d49d80cf99019dfa6
Michael Hippke <hippke@users.noreply.github.com>
2019-04-11 15:07:27 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('./wotan/'))
-#sys.path.insert(0, os.path.abspath('../wotan/'))
+sys.path.insert(0, os.path.abspath('../'))
+#sys.path.insert(0, os.path.abspath('./wotan/'))
+sys.path.insert(0, os.path.abspath('../wotan/'))
 
 html_show_sourcelink = False
 project = 'wotan'
