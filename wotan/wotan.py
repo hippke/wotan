11be298ba72554efe615e2b4107b7dff02e23648
Michael Hippke <hippke@users.noreply.github.com>
2019-04-15 20:44:20 +0200

    Update wotan.py

--- wotan/wotan.py
+++ wotan/wotan.py
@@ -1,30 +1,85 @@
 """Wotan is a free and open source algorithm to automagically remove stellar trends
 from light curves for exoplanet transit detection.
 """
+
 from __future__ import division
-import scipy.interpolate
+from numpy import float128 as TransformerMixin  # dummy import, replaced later
 import numpy
-import statsmodels.api
-from numpy import mean, median, array, abs, sort, inf, sin, exp, sum, pi, min, max, \
-    full, append, concatenate, diff, where, add, float32, nan, isnan, linspace, cos
+from numpy import mean, median, array, sort, inf, sin, exp, pi, full, append, \
+    concatenate, diff, where, add, float32, nan, isnan, linspace, cos, ones, zeros
 from numba import jit
-from sklearn.base import TransformerMixin
-from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import HuberRegressor
-from supersmoother import SuperSmoother as supersmoother
 from scipy.optimize import leastsq
+import scipy.interpolate
+
+
+@jit(fastmath=True, nopython=True, cache=True)
+def location_iter(data, cval, ftol, method_code):
+    """Robust location estimators"""
+
+    # Numba can't handle strings, so we're passing the location estimator as an int:
+    # 1 : biweight
+    # 2 : andrewsinewave
+    # 3 : welsch
+    # (the others are not relevant for location_iter)
+
+    # Initial estimate for the central location
+    delta_center = inf
+    median_data = median(data)
+    mad = median(numpy.abs(data - median_data))
+    center = center_old = median_data
+
+    # Neglecting this case was a bug in scikit-learn
+    if mad == 0:
+        return center
+
+    # one expensive division here, instead of two per loop later
+    cmad = 1 / (cval * mad)
+
+    # Newton-Raphson iteration, where each result is taken as the initial value of the
+    # next iteration. Stops when the difference of a round is below ``ftol`` threshold
+    while numpy.abs(delta_center) > ftol:
+        distance = data - center
+        dmad = distance * cmad
+
+        # Inlier weights
+        # biweight
+        if method_code == 1:
+            weight = (1 - dmad ** 2) ** 2
+        # andrewsinewave
+        if method_code == 2:
+            # avoid division by zero
+            dmad[dmad == 0] = 1e-10
+            weight = sin(dmad) / dmad
+        # welsch
+        if method_code == 3:
+            weight = exp(-(dmad**2) / 2)
+
+        # Outliers with weight zero
+        # biweight or welsch
+        if method_code == 1 or method_code == 3:
+            weight[(numpy.abs(dmad) >= 1)] = 0
+        # andrewsinewave
+        else:
+            weight[(numpy.abs(dmad) >= pi)] = 0
+
+        center += numpy.sum(distance * weight) / numpy.sum(weight)
+
+        # Calculate how much center moved to check convergence threshold
+        delta_center = center_old - center
+        center_old = center
+    return center
 
 
 @jit(fastmath=True, nopython=True, cache=True)
 def cofiam_detrend_curve(t, fact, D_max, k_max):
     ph = 2 * pi * t / D_max
-    detr = numpy.ones(len(t)) * fact[0]
+    detr = ones(len(t)) * fact[0]
     for k in range(1, k_max + 1):
         detr += fact[2 * k - 1] * sin(ph * k) + fact[2 * k] * cos(ph * k)
     return detr
 
 
-def detrend_cofiam(t, y, ferr, window, ftol):
+def detrend_cofiam(t, y, ferr, window_length, ftol):
 
     def find_detrending_for_region(t, y, ferr, k_max, D_max):
 
@@ -34,14 +89,14 @@ def detrend_cofiam(t, y, ferr, window, ftol):
         def out_func(blub):
             return cofiam_detrend_curve(t, best_param, D_max, k_max)
 
-        x0 = numpy.zeros(2 * k_max + 1)
+        x0 = zeros(2 * k_max + 1)
         x0[0] = mean(y)
         best_param, param_fit_status = leastsq(fit_func, x0, ftol=ftol)
         result = cofiam_detrend_curve(t, best_param, D_max, k_max)
         return out_func
 
     D_max = 2 * (max(t) - min(t))
-    k_max = max(1, min(100, int(D_max / window)))
+    k_max = max(1, min(100, int(D_max / window_length)))
     dw_previous = inf
     dw_mask = array([True] * len(t))
     for k_m in range(1, k_max + 1):
@@ -50,12 +105,11 @@ def detrend_cofiam(t, y, ferr, window, ftol):
 
         # Durbin-Watson autocorrelation statistics
         dw_y = (y[dw_mask] / trend[dw_mask] - 1)
-        dw = abs(sum((dw_y[1:] - dw_y[:-1]) ** 2) / (sum(dw_y ** 2)) - 2)
+        dw = numpy.abs(numpy.sum((dw_y[1:] - dw_y[:-1]) ** 2) / (numpy.sum(dw_y ** 2)) - 2)
 
         # If Durbin-Watson *increased* this round: Previous was the best
         if dw > dw_previous:   
             return trend
-        # print(k_m, k_max, dw)
         detrend_func = detrend_func_temp
         dw_previous = dw
     return detrend_func(t)
@@ -87,64 +141,6 @@ def location_hodges(data):
 
 
 @jit(fastmath=True, nopython=True, cache=True)
-def location_iter(data, cval, ftol, method_code):
-    """Robust location estimators"""
-
-    # Numba can't handle strings, so we're passing the location estimator as an int:
-    # 1 : biweight
-    # 2 : andrewsinewave
-    # 3 : welsch
-    # (the others are not relevant for location_iter)
-
-    # Initial estimate for the central location
-    delta_center = inf
-    median_data = median(data)
-    mad = median(abs(data - median_data))
-    center = center_old = median_data
-
-    # Neglecting this case was a bug in scikit-learn
-    if mad == 0:
-        return center
-
-    # one expensive division here, instead of two per loop later
-    cmad = 1 / (cval * mad)
-
-    # Newton-Raphson iteration, where each result is taken as the initial value of the
-    # next iteration. Stops when the difference of a round is below ``ftol`` threshold
-    while abs(delta_center) > ftol:
-        distance = data - center
-        dmad = distance * cmad
-
-        # Inlier weights
-        # biweight
-        if method_code == 1:
-            weight = (1 - dmad ** 2) ** 2
-        # andrewsinewave
-        if method_code == 2:
-            # avoid division by zero
-            dmad[dmad == 0] = 1e-10
-            weight = sin(dmad) / dmad
-        # welsch
-        if method_code == 3:
-            weight = exp(-(dmad**2) / 2)
-
-        # Outliers with weight zero
-        # biweight or welsch
-        if method_code == 1 or method_code == 3:
-            weight[(abs(dmad) >= 1)] = 0
-        # andrewsinewave
-        else:
-            weight[(abs(dmad) >= pi)] = 0
-
-        center += sum(distance * weight) / sum(weight)
-
-        # Calculate how much center moved to check convergence threshold
-        delta_center = center_old - center
-        center_old = center
-    return center
-
-
-@jit(fastmath=True, nopython=True, cache=True)
 def running_segment(time, flux, window_length, edge_cutoff, cval, ftol, method_code):
     """Iterator for a single time-series segment using time-series window sliders"""
 
@@ -165,8 +161,8 @@ def running_segment(time, flux, window_length, edge_cutoff, cval, ftol, method_c
         edge_cutoff = half_window
 
     # Pre-calculate border checks before entering the loop (reason: large speed gain)
-    low_index = min(time) + edge_cutoff
-    hi_index = max(time) - edge_cutoff
+    low_index = numpy.min(time) + edge_cutoff
+    hi_index = numpy.max(time) - edge_cutoff
     idx_start = 0
     idx_end = 0
 
@@ -219,7 +215,7 @@ class BSplineFeatures(TransformerMixin):
 
     def transform(self, X):
         nsamples, nfeatures = X.shape
-        features = numpy.zeros((nsamples, nfeatures * self.nsplines))
+        features = zeros((nsamples, nfeatures * self.nsplines))
         for ispline, spline in enumerate(self.bsplines):
             istart = ispline * nfeatures
             iend = (ispline + 1) * nfeatures
@@ -229,7 +225,7 @@ class BSplineFeatures(TransformerMixin):
     def get_bspline_basis(self, knots, degree=3, periodic=False):
         """Get spline coefficients for each basis spline"""
         knots, coeffs, degree = scipy.interpolate.splrep(
-            knots, numpy.zeros(len(knots)), k=degree, per=periodic)
+            knots, zeros(len(knots)), k=degree, per=periodic)
         ncoeffs = len(coeffs)
         bsplines = []
         for ispline in range(len(knots)):
@@ -239,9 +235,9 @@ class BSplineFeatures(TransformerMixin):
 
 
 def huber_spline_segment(time, flux, knot_distance):
-    duration = max(time) - min(time)
+    duration = numpy.max(time) - numpy.min(time)
     no_knots = int(duration / knot_distance)
-    knots = linspace(min(time), max(time), no_knots)
+    knots = linspace(numpy.min(time), numpy.max(time), no_knots)
     try:
         model = make_pipeline(
             BSplineFeatures(knots),
@@ -281,8 +277,8 @@ def flatten(time, flux, window_length=None, edge_cutoff=0, break_tolerance=None,
         Determines detrending method and location estimator. A time-windowed slider is
         invoked for location estimators `median`, `biweight`, `hodges`, `welsch`,
         `andrewsinewave`, `mean`, or `trim_mean`. Spline-based detrending is performed
-        for `huberspline` and `untrendy`. A locally weighted scatterplot smoothing is
-        performed for `lowess`.
+        for `huberspline`. A locally weighted scatterplot smoothing is performed for 
+        `lowess`.
     break_tolerance : float, default: window_length/2
         If there are large gaps in time (larger than ``window_length``/2), flatten will
         split the flux into several sub-lightcurves and apply the filter to each
@@ -319,10 +315,9 @@ def flatten(time, flux, window_length=None, edge_cutoff=0, break_tolerance=None,
     trend_flux : array-like
         Trend in the flux. Only returned if ``return_trend`` is `True`.
     """
-    if (
-    method
-    not in "biweight andrewsinewave welsch hodges median mean trim_mean huberspline cofiam untrendy"
-    ):
+    methods = "biweight lowess andrewsinewave welsch hodges median mean trim_mean \
+        huberspline cofiam"
+    if method not in methods:
         raise ValueError('Unknown detrending method')
 
     # Numba can't handle strings, so we're passing the location estimator as an int:
@@ -401,24 +396,39 @@ def flatten(time, flux, window_length=None, edge_cutoff=0, break_tolerance=None,
                 ftol,
                 method_code)
         elif method == 'lowess':
+            try:
+                import statsmodels.api
+            except:
+                raise ImportError('Could not import statsmodels')
             trend_segment = statsmodels.api.nonparametric.lowess(
                 endog=flux_view,
                 exog=time_view,
-                frac=window_length / (max(time_compressed) - min(time_compressed)),
+                frac=window_length / (numpy.max(time_compressed) - numpy.min(time_compressed)),
                 missing='none',
                 return_sorted=False
                 )
         elif method == 'huberspline':
+            try:
+                from sklearn.base import TransformerMixin
+                from sklearn.pipeline import make_pipeline
+                from sklearn.linear_model import HuberRegressor
+            except:
+                raise ImportError('Could not import sklearn')
             trend_segment = huber_spline_segment(
                 time_view,
                 flux_view,
                 knot_distance=window_length)
         elif method == 'supersmoother':
+            try:
+                from supersmoother import SuperSmoother as supersmoother
+            except:
+                raise ImportError('Could not import supersmoother')
             trend_segment = supersmoother(alpha=supersmoother_alpha).fit(
                 time_view, flux_view,).predict(time_view)
         elif method == 'cofiam':
             trend_segment = detrend_cofiam(
-                time_view, flux_view, numpy.ones(len(time_view)), window_length, ftol)
+                time_view, flux_view, ones(len(time_view)), window_length, ftol)
+
         trend_flux = append(trend_flux, trend_segment)
 
     # Insert results of non-NaNs into original data stream
