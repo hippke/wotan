9be67513bb50e6aace6ab7595c7854abc4b3d9e7
Michael Hippke <hippke@users.noreply.github.com>
2019-04-16 10:14:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 from os import path
 
+# Pull TLS version from single source of truth file
+try:  # Python 2
+    execfile(path.join("transitleastsquares", 'version.py'))
+except:  # Python 3
+    exec(open(path.join("transitleastsquares", 'version.py')).read())
+
 # If Python3: Add "README.md" to setup. 
 # Useful for PyPI (pip install wotan). Irrelevant for users using Python2
 try:
@@ -10,9 +16,9 @@ try:
 except:
     long_description = ' '
 
-    
-setup(name='wotan',
-    version=1,
+setup(
+    name='wotan',
+    version=WOTAN_VERSIONING,
     description='Wotan is a free and open source algorithm to automagically remove stellar trends from light curves for exoplanet transit detection',
     long_description=long_description,
     long_description_content_type='text/markdown',
