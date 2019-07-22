0591b04c6b4fdf0f951436b89065f38ca7e0c616
Michael Hippke <hippke@users.noreply.github.com>
2019-07-22 16:01:42 +0200

    Update gp.py

--- wotan/gp.py
+++ wotan/gp.py
@@ -5,7 +5,7 @@ from scipy.signal import lombscargle
 from wotan.helpers import cleaned_array
 
 
-def make_gp(time, flux, kernel, kernel_size, kernel_period, robust):
+def make_gp(time, flux, mask, kernel, kernel_size, kernel_period, robust):
     try:
         from sklearn.gaussian_process import GaussianProcessRegressor
         from sklearn.gaussian_process.kernels import RBF, Matern, ExpSineSquared
@@ -19,17 +19,20 @@ def make_gp(time, flux, kernel, kernel_size, kernel_period, robust):
     if kernel_size <= 0 or kernel_size >= float("inf"):
         raise ValueError('kernel_size must be finite and positive')
 
+    masked_flux = flux[mask==1]
+    masked_time = time[mask==1]
+
     # GPs need flux near zero, otherwise they often fail to converge
     # So, we normalize by some constant (the median) and later transform back
-    offset = numpy.median(flux)
-    flux -= offset
+    offset = numpy.median(masked_flux)
+    masked_flux -= offset
 
     # RBF and matern kernels are similar when matern's (kernel_size * 1000)
     if kernel == 'matern':
         kernel_size *= 1000
 
     kernel_size_bounds = (0.5 * kernel_size, 2 * kernel_size)
-    grid = time.reshape(-1, 1)
+    grid = masked_time.reshape(-1, 1)
 
     if kernel is None or kernel == 'squared_exp':
         use_kernel = RBF(kernel_size, kernel_size_bounds)
@@ -37,42 +40,43 @@ def make_gp(time, flux, kernel, kernel_size, kernel_period, robust):
     if kernel == 'matern':
         use_kernel = Matern(kernel_size, kernel_size_bounds, nu=3/2)
 
-    # Single pass or iteratively clipped
+    # non-periodic
     if kernel == 'matern' or kernel == 'squared_exp' or kernel is None:
-        if robust:
-            converged = False
-            newflux = flux.copy()
-            newtime = time.copy()
-            detrended_flux = flux.copy()
-            for i in range(constants.PSPLINES_MAXITER):
-                # Flux must be ~1. First round may by ~0. Then correct: 
-                if abs(numpy.median(detrended_flux)) < 0.5:
-                    detrended_flux += 1
-                mask_outliers = numpy.ma.where(
-                    1-detrended_flux < constants.PSPLINES_STDEV_CUT * numpy.std(
-                        detrended_flux))
-                newtime, newflux = cleaned_array(
-                    newtime[mask_outliers], newflux[mask_outliers])
-                grid = newtime.reshape(-1, 1)
-                GP = GaussianProcessRegressor(use_kernel).fit(grid, newflux)
-                trend_segment = GP.predict(grid)
-                detrended_flux = (newflux + offset) / (trend_segment + offset)
-                mask_outliers = numpy.ma.where(
-                    1-detrended_flux > constants.PSPLINES_STDEV_CUT * numpy.std(
-                        detrended_flux))
-                print('Iteration:', i + 1, 'Rejected outliers:', len(mask_outliers[0]))
-                # Check convergence
-                if converged or len(mask_outliers[0]) == 0:
-                    print('Converged.')
-                    break
-            # Final iteration, applied to unclipped time series 
-            # (interpolated over clipped values)
-            trend_segment = GP.predict(time.reshape(-1, 1))
+        if not robust:
+            converged = True
         else:
-            trend_segment = GaussianProcessRegressor(
-                use_kernel).fit(grid, flux).predict(grid)
+            converged = False
+        newflux = masked_flux.copy()
+        newtime = masked_time.copy()
+        detrended_flux = masked_flux.copy()
+        for i in range(constants.PSPLINES_MAXITER):
+            # Flux must be ~1. First round may by ~0. Then correct: 
+            if abs(numpy.median(detrended_flux)) < 0.5:
+                detrended_flux += 1
+            mask_outliers = numpy.ma.where(
+                1-detrended_flux < constants.PSPLINES_STDEV_CUT * numpy.std(
+                    detrended_flux))
+            newtime, newflux = cleaned_array(
+                newtime[mask_outliers], newflux[mask_outliers])
+            grid = newtime.reshape(-1, 1)
+            GP = GaussianProcessRegressor(use_kernel).fit(grid, newflux)
+            trend_segment = GP.predict(grid)
+            detrended_flux = (newflux + offset) / (trend_segment + offset)
+            mask_outliers = numpy.ma.where(
+                1-detrended_flux > constants.PSPLINES_STDEV_CUT * numpy.std(
+                    detrended_flux))
+            # Check convergence
+            if converged or len(mask_outliers[0]) == 0:
+                if robust:
+                    print('Converged.')
+                break
+            else:
+                print('Iteration:', i + 1, 'Rejected outliers:', len(mask_outliers[0]))
+        # Final iteration, applied to unclipped time series 
+        # (interpolated over clipped values)
+        trend_segment = GP.predict(time.reshape(-1, 1))
 
-    # Single pass; currently no iterative periodic kernel implemented
+    # periodic single pass; currently no iterative periodic kernel implemented
     if 'periodic' in kernel:
         # Determine most significant period
         if kernel == 'periodic_auto':
