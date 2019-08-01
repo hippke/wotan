f0c9f257faaf5661073e74b353ca37e0861d00f0
Michael Hippke <hippke@users.noreply.github.com>
2019-08-01 13:09:19 +0200

    add mask

--- wotan/iter_spline.py
+++ wotan/iter_spline.py
@@ -1,31 +1,29 @@
 from __future__ import print_function, division
-import numpy
+import numpy as np
 import wotan.constants as constants
 from wotan.helpers import cleaned_array
-from scipy import interpolate
+from scipy.interpolate import LSQUnivariateSpline
 
 
-def iter_spline(time, flux, window_length):
+def iter_spline(time, flux, mask, window_length):
+    masked_flux = flux[mask==1]
+    masked_time = time[mask==1]
     no_knots = (max(time) - min(time)) / window_length
-    newflux = flux.copy()
-    newtime = time.copy()
-    detrended_flux = flux.copy()
+    newflux = masked_flux.copy()
+    newtime = masked_time.copy()
+    newtime, newflux = cleaned_array(newtime, newflux)
+    detrended_flux = masked_flux.copy()
     for i in range(constants.PSPLINES_MAXITER):
-        mask_outliers = numpy.ma.where(
-            1 - detrended_flux < constants.PSPLINES_STDEV_CUT * numpy.std(detrended_flux))
+        outliers = 1 - detrended_flux < constants.PSPLINES_STDEV_CUT * np.std(detrended_flux)
+        mask_outliers = np.ma.where(outliers)
         newtime, newflux = cleaned_array(newtime[mask_outliers], newflux[mask_outliers])
         # knots must not be at the edges, so we take them as [1:-1]
-        knots = numpy.linspace(min(newtime), max(newtime), no_knots)[1:-1]
-        s = interpolate.LSQUnivariateSpline(newtime, newflux, knots)
-        trend_segment = s(newtime)
+        knots = np.linspace(min(newtime), max(newtime), no_knots)[1:-1]
+        trend = LSQUnivariateSpline(newtime, newflux, knots)
+        trend_segment = trend(newtime)
         detrended_flux = newflux / trend_segment
-        mask_outliers = numpy.ma.where(
-            1 - detrended_flux > constants.PSPLINES_STDEV_CUT * numpy.std(detrended_flux))
-        print('Iteration:', i + 1, 'Rejected outliers:', len(mask_outliers[0]))
-        # Check convergence
+        outliers = 1 - detrended_flux > constants.PSPLINES_STDEV_CUT * np.std(detrended_flux)
+        mask_outliers = np.ma.where(mask_outliers)
         if len(mask_outliers[0]) == 0:
-            print('Converged.')
             break
-    # Final iteration applied all data interpolated over clipped values
-    trend_segment = s(time)
-    return trend_segment
+    return trend(time)  # Final iteration: apply trend to clipped and masked values
